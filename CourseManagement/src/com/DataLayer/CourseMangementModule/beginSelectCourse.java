81da09b9c4f27a3af09ce08a90ababd614278f05
imguang <342601183@qq.com>
2015-11-15 21:52:56 +0800

    addstate

---
+++ CourseManagement/src/com/DataLayer/CourseMangementModule/beginSelectCourse.java
@@ -0,0 +1,82 @@
+package com.DataLayer.CourseMangementModule;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.http.NameValuePair;
+import org.apache.http.message.BasicNameValuePair;
+
+import com.DataLayer.Runnable.threadAdapterForPost;
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+/**
+ * @parameter *
+ */
+public class beginSelectCourse {
+
+	private String periodid;
+	private String starttime;
+	private String deadline;
+	private String flag;
+
+	private static String URL = "http://test.micromi.net/service/CourseManagement/beginSelectCourse.php";
+
+	public String getPeriodid() {
+		return periodid;
+	}
+
+	public void setPeriodid(String periodid) {
+		this.periodid = periodid;
+	}
+
+	public String getStarttime() {
+		return starttime;
+	}
+
+	public void setStarttime(String starttime) {
+		this.starttime = starttime;
+	}
+
+	public String getDeadline() {
+		return deadline;
+	}
+
+	public void setDeadline(String deadline) {
+		this.deadline = deadline;
+	}
+
+	public String docomfirm() {
+		// init
+		String result = "";
+		Gson gson = new Gson();
+
+		// pre
+		threadAdapterForPost nthread = new threadAdapterForPost();
+		List<NameValuePair> params = new ArrayList<NameValuePair>();
+		NameValuePair pair1 = new BasicNameValuePair("periodid", periodid);
+		NameValuePair pair2 = new BasicNameValuePair("starttime", starttime);
+		NameValuePair pair3 = new BasicNameValuePair("deadline", deadline);
+		NameValuePair pair4 = new BasicNameValuePair("deadline", flag);
+		params.add(pair1);
+		params.add(pair2);
+		params.add(pair3);
+		params.add(pair4);
+		nthread.setUrlString(URL);
+		nthread.setParams(params);
+
+		// doit
+		nthread.start();
+		try {
+			nthread.join();
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		// result
+		result = nthread.getJsonObject().get("return_flag").toString();
+		return result;
+	}
+
+}
