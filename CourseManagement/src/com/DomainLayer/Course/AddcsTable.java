dcc0daaebff9b1c8ca90a2488b1e505c6aa84684
rjgc2015 <550296705@qq.com>
2015-11-15 22:09:18 +0800

    8

--- CourseManagement/src/com/DomainLayer/Course/AddcsTable.java
+++ CourseManagement/src/com/DomainLayer/Course/AddcsTable.java
@@ -4,21 +4,31 @@ import java.util.ArrayList;
 import java.util.List;
 
 import com.DUtils.FileUtils;
-import com.DataLayer.CourseMangementModule.addCourses;
+import com.DataLayer.CourseMangementModule.addPeriod;
 import com.DataLayer.CourseMangementModule.doUpload;
 import com.DataLayer.CourseMangementModule.getInfoFromExcelAboutCourses;
 import com.DataLayer.CourseMangementModule.queryMajorState;
 import com.DataLayer.Model.majorState;
+import com.DomainLayer.Course.Ccoursemage.MessageItem;
+import com.DomainLayer.Course.Ccoursemage.SlideAdapter;
+import com.UIxml.ListViewCompat1;
 import com.control.R;
 
+import android.R.integer;
 import android.app.Activity;
-import android.bluetooth.BluetoothClass.Device.Major;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.Window;
 import android.view.View.OnClickListener;
 import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class AddcsTable extends Activity implements OnClickListener {
@@ -31,15 +41,23 @@ public class AddcsTable extends Activity implements OnClickListener {
 	Button btsx;
 	Button btwl;
 	Button btxa;
+	Button back;
+	Button settime;
 	private String majorid;
 	private String periodid;
 	private String path;
+	private int btid;
+	DatePicker begin;
+	DatePicker close;
+	TextView bgtest;
+	TextView cltest;
 
 	private static final int FILE_SELECT_CODE = 1;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		requestWindowFeature(Window.FEATURE_NO_TITLE);
 		setContentView(R.layout.addcstable);
 
 		btjs = (Button) findViewById(R.id.jsjsy);
@@ -50,6 +68,85 @@ public class AddcsTable extends Activity implements OnClickListener {
 		btsx = (Button) findViewById(R.id.sx);
 		btwl = (Button) findViewById(R.id.wlgc);
 		btxa = (Button) findViewById(R.id.xxaq);
+		back = (Button) findViewById(R.id.title_cancel);
+		settime = (Button) findViewById(R.id.settime);
+		// 返回上一级
+		back.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				onBackPressed();
+				overridePendingTransition(android.R.anim.slide_in_left,
+						android.R.anim.slide_out_right); // 切换动画
+
+			}
+		});
+		settime.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				AlertDialog.Builder builder = new AlertDialog.Builder(
+						AddcsTable.this);
+				LayoutInflater factory = LayoutInflater.from(AddcsTable.this);
+				final View textEntryView = factory.inflate(
+						R.layout.date_dialog, null);
+				builder.setTitle("设置时间");
+				builder.setView(textEntryView);
+				builder.setPositiveButton("确定",
+						new DialogInterface.OnClickListener() {
+							public void onClick(DialogInterface dialog,
+									int whichButton) {
+								// 事件处理
+								begin = (DatePicker) textEntryView
+										.findViewById(R.id.begin);
+								close = (DatePicker) textEntryView
+										.findViewById(R.id.close);
+								bgtest = (TextView) findViewById(R.id.begintext);
+								cltest = (TextView) findViewById(R.id.closetext);
+								int y1, y2, m1, m2, d1, d2;
+								y1 = begin.getYear();
+								y2 = close.getYear();
+								m1 = begin.getMonth();
+								m2 = close.getMonth();
+								d1 = begin.getDayOfMonth();
+								d2 = close.getDayOfMonth();
+								if (y2 > y1) {
+									bgtest.setText("开始时间：" + y1 + "-" + m1
+											+ "-" + d1);
+									cltest.setText("结束时间：" + y2 + "-" + m2
+											+ "-" + d2);
+
+								} else if (m2 > m1) {
+									bgtest.setText("开始时间：" + y1 + "-" + m1
+											+ "-" + d1);
+									cltest.setText("结束时间：" + y2 + "-" + m2
+											+ "-" + d2);
+
+								} else if (d2 > d1) {
+									bgtest.setText("开始时间：" + y1 + "-" + m1
+											+ "-" + d1);
+									cltest.setText("结束时间：" + y2 + "-" + m2
+											+ "-" + d2);
+
+								} else {
+									Toast.makeText(getApplicationContext(),
+											"开始时间不能大于结束时间，请重新输入！", 200).show();
+								}
+
+							}
+
+						});
+				builder.setNegativeButton("取消",
+						new DialogInterface.OnClickListener() {
+							public void onClick(DialogInterface dialog,
+									int whichButton) {
+
+							}
+						});
+				builder.create().show();
+
+			}
+		});
 
 		Intent intent = getIntent();
 		queryMajorState major = new queryMajorState();
@@ -109,7 +206,7 @@ public class AddcsTable extends Activity implements OnClickListener {
 	@Override
 	public void onClick(View v) {
 		// TODO Auto-generated method stub
-
+		btid = v.getId();
 		switch (v.getId()) {
 		case R.id.jsjsy:
 			majorid = "0";
@@ -153,13 +250,20 @@ public class AddcsTable extends Activity implements OnClickListener {
 	}
 
 	@Override
+	protected void onResume() {
+		// TODO Auto-generated method stub
+		super.onResume();
+
+	}
+
+	@Override
 	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
 		switch (requestCode) {
 		case FILE_SELECT_CODE:
 			if (resultCode == RESULT_OK) {
 				// Get the Uri of the selected file
 				Uri uri = data.getData();
-				String path = FileUtils.getPath(this, uri);
+				path = FileUtils.getPath(this, uri);
 				// 上传课表
 				String a = path;
 				String b = a.substring(a.lastIndexOf("/") + 1, a.length());
@@ -184,13 +288,20 @@ public class AddcsTable extends Activity implements OnClickListener {
 					e.printStackTrace();
 				}
 				String result = getInfo.getResult();
-				if(result.equals("")){}//TODO
+				if (result.equals("y1")) {
+					Toast.makeText(getApplicationContext(), "插入成功！", 200)
+							.show();
+					Button temBt = (Button) findViewById(btid);
+					temBt.setBackgroundColor(getResources().getColor(
+							R.color.green));
+				} else {
+					Toast.makeText(getApplicationContext(), "插入失败！", 200)
+							.show();
+				}
 				break;
 			}
 			super.onActivityResult(requestCode, resultCode, data);
 		}
 	}
 
-	
-	
 }
