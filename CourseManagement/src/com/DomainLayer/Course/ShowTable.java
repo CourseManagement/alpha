1c684275476b348c72b6fd675a86a02289eb94ab
rjgc2015 <550296705@qq.com>
2015-11-18 15:28:22 +0800

    reset course

--- CourseManagement/src/com/DomainLayer/Course/ShowTable.java
+++ CourseManagement/src/com/DomainLayer/Course/ShowTable.java
@@ -1,16 +1,34 @@
 package com.DomainLayer.Course;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import com.DataLayer.Model.courseInfo;
 import com.control.R;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.ListView;
 
 public class ShowTable extends Activity {
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
+		setContentView(R.layout.coursemanage);
+		
+		Intent intent = getIntent();
+		List<courseInfo> lvcourseInfo = new ArrayList<courseInfo>();
+		ListView lv = (ListView) this.findViewById(R.id.lvcourse);
+		List<String> str = new ArrayList<String>();
+		
+		
+		
+		
+		
+		
+		
 	}
 
 }
